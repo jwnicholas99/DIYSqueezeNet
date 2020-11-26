c2168287c414b10a02b57929367c62454b1f94d3
lithium710 <leonard.wt.so@gmail.com>
2020-11-26 10:48:55 +0800

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -9,48 +9,38 @@ def unpickle(file):
         dict = pickle.load(fo)
     return dict
 
-def load_databatch(data_folder, idx, img_size=32):
-    data_file = os.path.join(data_folder, 'train_data_batch_')
+def load_databatch(data_folder, idx, img_size=8, is_train=True):
+    if is_train:
+        data_file = os.path.join(data_folder, 'train_data_batch_')
+        d = unpickle(data_file + str(idx))
+    else:
+        data_file = os.path.join(data_folder, 'val_data')
 
-    d = unpickle(data_file + str(idx))
+    
     x = d['data']
     y = d['labels']
     data_size = x.shape[0]
-    X_train = x
     Y_train = np.array(y)
-    """mean_image = d['mean']
 
-    x = x/np.float32(255)
-    mean_image = mean_image/np.float32(255)
+    return (tf.reshape(tf.convert_to_tensor(x, dtype=tf.float64), shape=[data_size, img_size, img_size, 3]),
+            tf.convert_to_tensor(Y_train, dtype=tf.string))
 
-    # Labels are indexed from 1, shift it so that indexes start at 0
-    y = [i-1 for i in y]
+def get_data(train_file_path, test_file_path, batch_size=1000):
+    train_set = train_file_path
+    for i in range(10):
+        train_data = tf.data.Dataset.from_generator(load_databatch, output_types=(tf.float64, tf.string),
+                                                    args=(tf.Constant(train_file_path), tf.Constant(i+1)))
+        if type(train_set) == str:
+            train_set = train_data
+        else:
+            train_set.concatenate(train_data)
+    validation_data = tf.data.Dataset.from_generator(load_databatch, output_types=(tf.float64, tf.string),
+                                                    args=(tf.Constant(test_file_path)))
+    train_set = train_set.shuffle()
+    train_set = train_set.batch(batch_size)
+    validation_data = validation_data.batch(batch_size)
     
-
-    x -= mean_image
-
-    img_size2 = img_size * img_size
-
-    x = np.dstack((x[:, :img_size2], x[:, img_size2:2*img_size2], x[:, 2*img_size2:]))
-    x = x.reshape((x.shape[0], img_size, img_size, 3)).transpose(0, 3, 1, 2)
-
-    # create mirrored images
-    X_train = x[0:data_size, :, :, :]
-    Y_train = y[0:data_size]
-    X_train_flip = X_train[:, :, :, ::-1]
-    Y_train_flip = Y_train
-    X_train = np.concatenate((X_train, X_train_flip), axis=0)
-    Y_train = np.concatenate((Y_train, Y_train_flip), axis=0)"""
-
-    return tf.reshape(tf.convert_to_tensor(X_train), shape=[data_size, 8, 8, 3]), Y_train.astype('int32')#,
-        #mean=mean_image)
-
-def get_data(file_path):
-    #train_data = tf.io.read_file(file_path)
-    #print(train_data)
-    x_train, y_train = load_databatch('Imagenet8_train', 1)
-    
-    train_datagen = ImagePreprocess.ImageDataGenerator(
+    """train_datagen = ImagePreprocess.ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
         zoom_range=0.2,
@@ -58,33 +48,24 @@ def get_data(file_path):
         vertical_flip=True,
         rotation_range=30)
     test_datagen = ImagePreprocess.ImageDataGenerator(rescale=1./255)
-    train_datagen.fit(x_train)
-    print(x_train[:10])
-
-    """train_generator = train_datagen.flow_from_directory(
-        'Imagenet8_train',
-        target_size=(150, 150),
-        batch_size=32,
-        class_mode='binary')"""
     
-    """validation_generator = test_datagen.flow_from_directory(
-        'data/validation',
+
+    train_generator = train_datagen.flow(
+        train_data[:, 0], train_data[:, 1],
         target_size=(150, 150),
         batch_size=32,
-        class_mode='binary')"""
-    #return train_generator, validation_generator
+        class_mode='categorical')
+    
+    validation_generator = test_datagen.flow(
+        validation_data[:, 0], validation_data[:, 1],
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='categorical')
+    """
+    train_set.concatenate(train_set.map(tf.image.random_flip_left_right))
+    train_set.concatenate(train_set.map(tf.image.random_flip_up_down))
+    
+    return train_set, validation_data
 
 if __name__ == "__main__":
-    #mydict = load_databatch('Imagenet8_train', 1)
-    #print(mydict)
-    get_data('asdf')
-
-
-    """
-    model.fit(
-        train_generator,
-        steps_per_epoch=2000,
-        epochs=50,
-        validation_data=validation_generator,
-        validation_steps=800)
-    """
+    train_gen, val_gen = get_data('Imagenet8_train', 'Imagenet8_val')
