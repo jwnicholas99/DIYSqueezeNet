918172bec6458fe06a56a0259af4f057a71d4756
jlim32 <jlim32@cs.brown.edu>
2020-11-27 23:30:29 -0500

    [FEAT] Implement Fire Module
    * Implement fire module as a keras layer so that can easily
      create fire modules.
    * Each fire module has 4 layers: squeeze, expand_1x1, expand_3x3
      and concat. We fire squeeze the inputs to decrease the number of
      filters. Then, we expand the squeezed inputs using 1x1 kernels
      and 3x3 kernels. Lastly, we concatenate the outputs of the 1x1
      expansion and 3x3 expansion layers depth-wise (filters).

---
+++ fire_mod.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import tf.keras as keras
+
+class FireLayer(tf.keras.layers.Layer):
+    def __init__(self, squeeze_filters, expand_filters):
+        """
+        :param squeeze_filters: num of filters after squeezing
+        :param expand_filters: num of filter for each expansion
+
+        Note that there are two types of expansion - 1x1 and 3x3 kernel,
+        so we will concatenate the outputs of both types of expansion 
+        depth-wise. This means that the number of output filters for eac
+        fire module is expand_filters * 2.
+        """
+        super(FireLayer, self).__init__()
+
+        # Initialize learnable params
+        self.squeeze = keras.layers.Convolution2D(squeeze_filters,
+                                                  (1,1),
+                                                  activation='relu',
+                                                  kernel_initializer='glorot_uniform',
+                                                  padding='same')
+
+        self.expand_1x1 = keras.layers.Convolution2D(expand_filters,
+                                                    (1,1),
+                                                    activation='relu',
+                                                    kernel_initializer='glorot_uniform',
+                                                    padding='same')
+
+        self.expand_3x3 = keras.layers.Convolution2D(expand_filters,
+                                                    (3,3),
+                                                    activation='relu',
+                                                    kernel_initializer='glorot_uniform',
+                                                    padding='same')
+
+        self.concat = keras.layers.Concatenate(axis=-1)
+
+    def call(self, inputs):
+        squeeze = self.squeeze(inputs)
+        expand_1x1 = self.expand_1x1(squeeze)
+        expand_3x3 = self.expand_3x3(squeeze)
+        concat = self.concat([expand_1x1, expand_3x3])
+
+        return concat
