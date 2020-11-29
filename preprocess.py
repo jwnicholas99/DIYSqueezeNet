80385b601993fe78689cc508ca32c4bfb6307b98
lithium710 <leonard.wt.so@gmail.com>
2020-11-29 18:20:18 +0800

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -2,14 +2,14 @@ import numpy as np
 import tensorflow as tf
 import pickle
 import os
-import tensorflow.keras.preprocessing.image as ImagePreprocess
+#import tensorflow.keras.preprocessing.image as ImagePreprocess
 
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo)
     return dict
 
-def load_databatch(data_folder, idx, img_size=8, is_train=True):
+def load_databatch(data_folder, idx, img_size=32, is_train=True):
     if is_train:
         data_file = os.path.join(data_folder, 'train_data_batch_')
         d = unpickle(data_file + str(idx))
@@ -19,23 +19,25 @@ def load_databatch(data_folder, idx, img_size=8, is_train=True):
     
     x = d['data']
     y = d['labels']
+    x = x/np.float32(255)
     data_size = x.shape[0]
-    Y_train = np.array(y)
 
     return (tf.reshape(tf.convert_to_tensor(x, dtype=tf.float64), shape=[data_size, img_size, img_size, 3]),
-            tf.convert_to_tensor(Y_train, dtype=tf.string))
+            tf.math.subtract(tf.convert_to_tensor(y, dtype=tf.int64), tf.Constant(1)))
 
 def get_data(train_file_path, test_file_path, batch_size=1000):
     train_set = train_file_path
     for i in range(10):
-        train_data = tf.data.Dataset.from_generator(load_databatch, output_types=(tf.float64, tf.string),
+        train_data = tf.data.Dataset.from_generator(load_databatch, output_types=(tf.float64, tf.int64),
                                                     args=(tf.Constant(train_file_path), tf.Constant(i+1)))
         if type(train_set) == str:
             train_set = train_data
         else:
             train_set.concatenate(train_data)
-    validation_data = tf.data.Dataset.from_generator(load_databatch, output_types=(tf.float64, tf.string),
+    validation_data = tf.data.Dataset.from_generator(load_databatch, output_types=(tf.float64, tf.int64),
                                                     args=(tf.Constant(test_file_path)))
+    train_set = train_set.map(tf.image.random_flip_left_right)
+    train_set = train_set.map(tf.image.random_flip_up_down)
     train_set = train_set.shuffle()
     train_set = train_set.batch(batch_size)
     validation_data = validation_data.batch(batch_size)
@@ -62,10 +64,8 @@ def get_data(train_file_path, test_file_path, batch_size=1000):
         batch_size=32,
         class_mode='categorical')
     """
-    train_set.concatenate(train_set.map(tf.image.random_flip_left_right))
-    train_set.concatenate(train_set.map(tf.image.random_flip_up_down))
     
     return train_set, validation_data
 
 if __name__ == "__main__":
-    train_gen, val_gen = get_data('Imagenet8_train', 'Imagenet8_val')
+    train_gen, val_gen = get_data('Imagenet32_train', 'Imagenet32_val')
