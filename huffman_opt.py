8abaa47df9cd98c2cb1d08cd8b9b44b03587c292
jlim32 <jwnicholas99@gmail.com>
2020-12-08 13:52:53 +0000

    [FEAT] Add compare_encoding_size function to huffman_opt.py

--- huffman_opt.py
+++ huffman_opt.py
@@ -13,8 +13,7 @@ from dahuffman import HuffmanCodec
 # Input: Tensor
 # Output: Encoded tensor, Codec
 def encode(weights):
-    assert(tf.is_tensor(weights))
-    flattened_array = weights.numpy().flatten()
+    flattened_array = weights.flatten()
     codec = HuffmanCodec.from_data(flattened_array)
     return codec.encode(flattened_array), codec
 
@@ -26,6 +25,13 @@ def decode(code, codec, shape=None):
         return np.reshape(codec.decode(code), shape)
     else:
         return codec.decode(code)
+    
+def compare_encoding_size(code, weights):
+    weights_size = 32 
+    for dim in weights.shape:
+        weights_size *= dim 
+    code_size = len(code) 
+    print("Weights tensor: {} bits. Code: {} bits.".format(weights_size, code_size), flush=True) 
 
 # Test Demo
 def demo():
@@ -37,7 +43,7 @@ def demo():
 
     # Syntax for encoding
     print("Encoding...", flush=True)
-    code, codec = encode(test_weights)
+    code, codec = encode(test_weights.numpy())
     print("Successfully encoded tensor to {} bytes.".format(len(code)), flush=True)
 
     # Syntax for decoding
@@ -46,7 +52,7 @@ def demo():
     print("Successfully decoded tensor into tensor of shape {} x {}.".format(output_weights.shape[0], output_weights.shape[1]), flush=True)
 
     # Making sure the encode and decode did not alter tensor
-    equality_check = np.all(tf.equal(test_weights, output_weights))
+    equality_check = np.all(np.equal(test_weights, output_weights))
     print("Result of equality check: {}".format(equality_check), flush=True)
 
     print("-" * 30)
